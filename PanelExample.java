31a0f065868f41e5a9604bde32d38dcbba670c79
salonigupta.my96@gmail.com <salonigupta.my96@gmail.com>
2017-07-08 23:14:02 +0530

    my java example

---
+++ PanelExample.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.awt.event.*;
+  
+public class PanelExample {  
+     PanelExample()  
+        {  
+        Frame f= new Frame("Panel Example");  
+
+			f.addWindowListener(new WindowAdapter(){		
+		
+		public void windowClosing(WindowEvent windowevent)
+		{
+			System.exit(0);
+		}
+		});
+        Panel panel=new Panel();  
+        panel.setBounds(40,80,200,200);    
+        panel.setBackground(Color.gray);  
+        Button b1=new Button("Button 1");     
+        b1.setBounds(50,100,80,30);    
+        b1.setBackground(Color.yellow);   
+        Button b2=new Button("Button 2");   
+        b2.setBounds(100,100,80,30);    
+        b2.setBackground(Color.green);   
+        panel.add(b1); panel.add(b2);  
+        f.add(panel);  
+        f.setSize(400,400);    
+        f.setLayout(null);    
+        f.setVisible(true);    
+        }  
+        public static void main(String args[])  
+        {  
+        new PanelExample();  
+        }  
+}  
