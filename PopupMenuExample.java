67f2f87f66305f450549a824d0ce8a075b7c9e6a
salonigupta.my96@gmail.com <salonigupta.my96@gmail.com>
2017-07-08 23:14:23 +0530

    my java example

---
+++ PopupMenuExample.java
@@ -0,0 +1,41 @@
+import java.awt.*;  
+import java.awt.event.*;  
+class PopupMenuExample  
+{  
+     PopupMenuExample(){  
+         final Frame f= new Frame("PopupMenu Example"); 
+
+		f.addWindowListener(new WindowAdapter(){		
+		
+		public void windowClosing(WindowEvent windowevent)
+		{
+			System.exit(0);
+		}
+		});
+
+		 
+         final PopupMenu popupmenu = new PopupMenu("Edit");   
+         MenuItem cut = new MenuItem("Cut");  
+         cut.setActionCommand("Cut");  
+         MenuItem copy = new MenuItem("Copy");  
+         copy.setActionCommand("Copy");  
+         MenuItem paste = new MenuItem("Paste");  
+         paste.setActionCommand("Paste");      
+         popupmenu.add(cut);  
+         popupmenu.add(copy);  
+         popupmenu.add(paste);        
+         f.addMouseListener(new MouseAdapter() {  
+            public void mouseClicked(MouseEvent e) {              
+                popupmenu.show(f , e.getX(), e.getY());  
+            }                 
+         });  
+         f.add(popupmenu);   
+         f.setSize(400,400);  
+         f.setLayout(null);  
+         f.setVisible(true);  
+     }  
+public static void main(String args[])  
+{  
+        new PopupMenuExample();  
+}  
+}  
